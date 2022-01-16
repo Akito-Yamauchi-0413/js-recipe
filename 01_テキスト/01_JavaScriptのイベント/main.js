8cb7de304dc4e54534e943a9a205a485c3b24f7a
Akito-Yamauchi-0413 <wantan2490@gmail.com>
2022-01-16 17:28:14 +0900

    [3の倍数]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+button.onclick = alertMessage
+
+//button.onclickには関数を代入しなければならないが、alerMessage()は関数ではないから
