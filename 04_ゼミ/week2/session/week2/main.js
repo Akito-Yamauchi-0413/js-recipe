e825cd5701afdb7a5ac06c03e29e03572bd9858d
Akito-Yamauchi-0413 <wantan2490@gmail.com>
2022-01-28 17:32:43 +0900

    発展問題要復習！

--- 04_ゼミ/week2/session/week2/main.js
+++ 04_ゼミ/week2/session/week2/main.js
@@ -5,8 +5,16 @@ const cardsContainer = document.getElementById("memo-container")
 addButton.onclick = function () {
   console.log(memoInput.value)
   const card = document.createElement("div")
+  const dcard = document.createElement("button")
   card.className = "memo-container"
   card.textContent = memoInput.value
+  dcard.textContent = "削除"
   cardsContainer.append(card)
+
+  card.append(dcard)
+
+  dcard.onclick = function () {
+    card.remove()
+  }
   memoInput.value = null
 }
