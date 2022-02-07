5cfeacff7e76d0d8038deb705dec327d1d7d8606
Akito-Yamauchi-0413 <wantan2490@gmail.com>
2022-02-08 04:49:33 +0900

    JavaScriptの基礎復習

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -1,17 +1,6 @@
 const hikakin = {
-  name: "ヒカキン",
-  age: 24,
-  hobbies: ["ゲーム", "動画編集", "猫と遊ぶこと"],
-  isHappy: true,
-  address: {
-    country: "日本",
-    city: "東京",
-  },
   sayHello: function () {
-    console.log("ブンブンハロー YouTube")
+    console.log("こんにちは")
   },
 }
-
-// 練習問題
-// hikakin オブジェクトに入っている sayHello メソッドを実行してください。
 hikakin.sayHello()
